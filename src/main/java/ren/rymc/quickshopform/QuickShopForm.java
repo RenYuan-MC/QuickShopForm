2621e5723d011d0ba7b603b115c9ce95eb012ddf
RENaa_FD <1593713272@qq.com>
2023-01-02 22:26:42 +0800

    [构建跳过] QuickShop-Hikari支持

--- src/main/java/ren/rymc/quickshopform/QuickShopForm.java
+++ src/main/java/ren/rymc/quickshopform/QuickShopForm.java
@@ -18,8 +18,8 @@ import org.geysermc.cumulus.response.CustomFormResponse;
 import org.geysermc.cumulus.response.SimpleFormResponse;
 import org.geysermc.cumulus.util.FormImage;
 import org.geysermc.floodgate.api.FloodgateApi;
-import org.maxgamer.quickshop.api.event.ShopClickEvent;
-import org.maxgamer.quickshop.api.shop.Shop;
+import com.ghostchu.quickshop.api.event.ShopClickEvent;
+import com.ghostchu.quickshop.api.shop.Shop;
 import ren.rymc.quickshopform.metrics.Metrics;
 import ren.rymc.quickshopform.utils.Utils;
 
@@ -27,64 +27,56 @@ import java.util.UUID;
 
 public final class QuickShopForm extends JavaPlugin implements Listener {
 
-    private Boolean shopClickStatus = false;
-
     @EventHandler(priority = EventPriority.HIGHEST)
     public void PlayerInteractEvent(PlayerInteractEvent event) {
+
         Player player = event.getPlayer();
-        UUID uuid = player.getUniqueId();
+        if (!FloodgateApi.getInstance().isFloodgatePlayer(player.getUniqueId())) return;
+
         Block block = event.getClickedBlock();
         if (block == null) return;
         Shop shop = Utils.getQuickShop(block);
         if (shop == null) return;
-        Action action = event.getAction();
-        if (!shopClickStatus) {
-            if (!FloodgateApi.getInstance().isFloodgatePlayer(uuid)) return;
-            if (action.equals(Action.RIGHT_CLICK_BLOCK)) {
-                if (block.getType().equals(Material.CHEST) || block.getType().equals(Material.TRAPPED_CHEST)) return;
-                if (player.isOp() || shop.getOwner().equals(player.getUniqueId())) {
-                    sendQuickShopSettingForm(player, shop);
-                }
 
-            }
+        Action action = event.getAction();
+
+        if (action.equals(Action.LEFT_CLICK_BLOCK)) {
+            if (player.isSneaking()) shop.openPreview(player);
             return;
         }
-        shopClickStatus = false;
-        if (!FloodgateApi.getInstance().isFloodgatePlayer(uuid)) return;
-        if (action.equals(Action.LEFT_CLICK_BLOCK)) {
-            if (player.isSneaking()) {
-                shop.openPreview(player);
-                return;
-            }
-            if (Utils.isQuickShopEmpty(shop)) return;
-            sendQuickShopMainForm(player, shop.getPrice());
 
+        if (!action.equals(Action.RIGHT_CLICK_BLOCK)) return;
+
+        if (block.getType().equals(Material.CHEST) || block.getType().equals(Material.TRAPPED_CHEST)) return;
+        if (player.isOp() || shop.getOwner().equals(player.getUniqueId())) {
+            sendQuickShopSettingForm(player, shop);
         }
+
     }
 
     @EventHandler(priority = EventPriority.HIGHEST)
     public void PlayerAnimationEvent(PlayerAnimationEvent event) {
+
         Player player = event.getPlayer();
+        if (!FloodgateApi.getInstance().isFloodgatePlayer(player.getUniqueId())) return;
+
         if (!player.getGameMode().equals(GameMode.ADVENTURE)) return;
-        UUID uuid = player.getUniqueId();
-        if (!FloodgateApi.getInstance().isFloodgatePlayer(uuid)) return;
+
         Block block = player.getTargetBlock(null, 5);
         Shop shop = Utils.getQuickShop(block);
         if (shop == null) return;
-        if (!shopClickStatus) {
-            if (player.isSneaking() && (!block.getType().equals(Material.CHEST) || !block.getType().equals(Material.TRAPPED_CHEST))) {
-                shop.openPreview(player);
-            }
-            return;
-        }
-        shopClickStatus = false;
-        sendQuickShopMainForm(player, shop.getPrice());
+
+        if (!player.isSneaking()) return;
+        if (block.getType().equals(Material.CHEST) || block.getType().equals(Material.TRAPPED_CHEST)) return;
+        shop.openPreview(player);
 
     }
 
-    @EventHandler
+    @EventHandler(ignoreCancelled = true)
     public void ShopClickEvent(ShopClickEvent event) {
-        shopClickStatus = true;
+        Shop shop = event.getShop();
+        if (Utils.isQuickShopEmpty(shop)) return;
+        sendQuickShopMainForm(event.getClicker(), shop.getPrice());
     }
 
     private void sendQuickShopSettingForm(Player player, Shop shop) {
--- src/main/java/ren/rymc/quickshopform/utils/Utils.java
+++ src/main/java/ren/rymc/quickshopform/utils/Utils.java
@@ -1,5 +1,7 @@
 package ren.rymc.quickshopform.utils;
 
+import com.ghostchu.quickshop.api.QuickShopAPI;
+import org.bukkit.Bukkit;
 import org.bukkit.block.Block;
 import org.bukkit.block.Chest;
 import org.bukkit.block.data.BlockData;
@@ -7,13 +9,20 @@ import org.bukkit.block.data.Directional;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
-import org.maxgamer.quickshop.QuickShop;
-import org.maxgamer.quickshop.api.shop.Shop;
-import org.maxgamer.quickshop.api.shop.ShopType;
+import com.ghostchu.quickshop.api.shop.Shop;
+import com.ghostchu.quickshop.api.shop.ShopType;
+import org.bukkit.plugin.Plugin;
 
 public class Utils {
+    private static QuickShopAPI quickShopAPI = null;
+
+    static {
+        Plugin plugin = Bukkit.getPluginManager().getPlugin("QuickShop-Hikari");
+        if (plugin != null && plugin.isEnabled()) quickShopAPI = (QuickShopAPI) plugin;
+    }
 
     public static Boolean isQuickShopEmpty(Shop shop) {
+        if (quickShopAPI == null) return false;
         if (shop == null) return false;
         if (shop.isUnlimited()) return false;
         Inventory shopInventory = ((Chest) shop.getLocation().getBlock().getState()).getBlockInventory();
@@ -25,15 +34,17 @@ public class Utils {
     }
 
     public static Shop getQuickShop(Block block) {
+        if (quickShopAPI == null) return null;
         if (block == null) return null;
-        Shop shop = QuickShop.getInstance().getShopManager().getShop(block.getLocation());
+        Shop shop = quickShopAPI.getShopManager().getShop(block.getLocation());
         if (shop != null) return shop;
         BlockData Drops = block.getBlockData();
         if (!(Drops instanceof Directional)) return null;
-        return QuickShop.getInstance().getShopManager().getShop(block.getRelative(((Directional) Drops).getFacing(),-1).getLocation());
+        return quickShopAPI.getShopManager().getShop(block.getRelative(((Directional) Drops).getFacing(),-1).getLocation());
     }
 
     public static void changeShopType(Player player, Shop shop) {
+        if (quickShopAPI == null) return;
         if (shop == null) return;
         if (!(player.isOp() || shop.getOwner().equals(player.getUniqueId()))) return;
         shop.setShopType(shop.isBuying() ? ShopType.SELLING : ShopType.BUYING);
